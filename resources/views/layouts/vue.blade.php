6176522bed490fbb8b6fb1ea6f9bf80e1a20cfad
leandro47 <leandrosilva47@live.com>
2021-08-19 19:19:41 -0300

    fixed path js

--- resources/views/layouts/vue.blade.php
+++ resources/views/layouts/vue.blade.php
@@ -24,17 +24,18 @@
     <div id="app"></div>
 
     <script src="{{ asset('js/app.js') }}"></script>
-    <script src="../../../assets/vendors/js/vendor.bundle.base.js"></script>>
-    <script src="../../../assets/vendors/select2/select2.min.js"></script>
-    <script src="../../assets/vendors/typeahead.js/typeahead.bundle.min.js"></script>
-    <script src="../../../assets/js/off-canvas.js"></script>
-    <script src="../../../assets/js/hoverable-collapse.js"></script>
-    <script src="../../../assets/js/misc.js"></script>
-    <script src="../../../assets/js/settings.js"></script>
-    <script src="../../../assets/js/todolist.js"></script>
-    <script src="../../../assets/js/file-upload.js"></script>
-    <script src="../../../assets/js/typeahead.js"></script>
-    <script src="../../../assets/js/select2.js"></script>
+    <script src="{{ asset('assets/vendors/js/vendor.bundle.base.js') }}"></script>
+
+    <script src="{{ asset('assets/vendors/select2/select2.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/typeahead.js/typeahead.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('assets/js/misc.js') }}"></script>
+    <script src="{{ asset('assets/js/settings.js') }}"></script>
+    <script src="{{ asset('assets/js/todolist.js') }}"></script>
+    <script src="{{ asset('assets/js/file-upload.js') }}"></script>
+    <script src="{{ asset('assets/js/typeahead.js') }}"></script>
+    <script src="{{ asset('assets/js/select2.js') }}"></script>
 </body>
 
 </html>
